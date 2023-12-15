6cc28d1854f7aa18d93dab18dcfc9c2fbdfc376e
Duc Trong <trong.phamtranduc@gmail.com>
2023-12-15 10:01:35 +0700

    update

--- shop-web-server/server.js
+++ shop-web-server/server.js
@@ -1,7 +1,7 @@
 const app = require('./src/app')
 const { disconnect } = require('./src/helpers/check.connect')
 
-const PORT = process.env.PORT || 3056
+const PORT = process.env.PORT || 8080
 
 const server = app.listen(PORT, () => {
     console.log(`WSV eCommerce start with ${PORT}`)
